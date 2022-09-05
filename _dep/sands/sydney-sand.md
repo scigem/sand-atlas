1c823d081c2e0c9311aa2ed7a2c78d2e0e11de61
Benjy Marks <benjy.marks@sydney.edu.au>
2022-09-05 22:12:33 +1000

    moved sand data to json file

--- _dep/sands/sydney-sand.md
+++ _dep/sands/sydney-sand.md
@@ -6,6 +6,7 @@ image: assets/images/gobi-desert-sand.jpg
 lat_lon: "-33.7964782,151.2846034"
 source_data_url: https://www.fake.link
 publication_url: https://www.fake.link
+download_url: https://www.fake.link
 license: <a href="https://creativecommons.org/licenses/by-sa/4.0/">CC BY-SA 4.0</a>
 size_range: 0.1 mm to 1 mm
 mineralogy: Quartz
---
+++ _plugins/makeSandPages.rb
@@ -0,0 +1,53 @@
+module SandPlugin
+    class SandPageGenerator < Jekyll::Generator
+      safe true
+  
+      def generate(site)
+        site.data['sands'].each do |page|
+          site.pages << SandPage.new(site, page)
+        end
+      end
+    end
+  
+    # Subclass of `Jekyll::Page` with custom method definitions.
+    class SandPage < Jekyll::Page
+      def initialize(site, page)
+        
+        @site = site             # the current site instance.
+        @base = site.source      # path to the source directory.
+        @dir  = page[0]          # the directory the page will reside in.
+  
+        # All pages have the same filename, so define attributes straight away.
+        @basename = 'index'      # filename without the extension.
+        @ext      = '.html'      # the extension.
+        @name     = 'index.html' # basically @basename + @ext.
+  
+        # Initialize data hash with a key pointing to all posts under current category.
+        # This allows accessing the list in a template via `page.linked_docs`.
+        @data = {
+          'data' => page[1],
+          'layout' => 'sand',
+          'image' => page[1]["image"],
+          'title' => page[1]["title"],
+          'description' => page[1]["description"],
+        }
+
+        
+  
+        # Look up front matter defaults scoped to type `categories`, if given key
+        # doesn't exist in the `data` hash.
+        data.default_proc = proc do |_, key|
+          site.frontmatter_defaults.find(relative_path, :categories, key)
+        end
+      end
+  
+      # Placeholders that are used in constructing page URL.
+      def url_placeholders
+        {
+          :path   => @dir,
+          :basename   => basename,
+          :output_ext => output_ext,
+        }
+      end
+    end
+  end
