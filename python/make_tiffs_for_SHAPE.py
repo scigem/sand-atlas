ec994d6aeb5867a0a91f04880366973107caf771
Benjamin Marks <benjy.marks@sydney.edu.au>
2024-08-09 16:06:35 +1000

    added dropdown menu for multiple download types

--- python/make_tiffs_for_SHAPE.py
+++ python/make_tiffs_for_SHAPE.py
@@ -4,6 +4,7 @@ import glob
 import numpy
 import tqdm
 import tifffile
+import nrrd
 
 foldername = sys.argv[1]
 files = glob.glob(foldername + '/*.npz')
@@ -16,3 +17,5 @@ for i, file in tqdm.tqdm(enumerate(files)):
     data = numpy.load(file, allow_pickle=True)
     this_particle = data["volume"]
     tifffile.imwrite(f'SHAPE/particle_{i}.tiff', this_particle.astype('uint8'))
+    # nrrd.write(f'SHAPE/particle_{i}.nrrd', this_particle.astype('uint8'))
+
